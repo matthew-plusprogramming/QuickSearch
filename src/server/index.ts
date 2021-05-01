3e03269e92d30737c72cb364676dba50be644749
matthew-plusprogramming <matthewlinplusprogramming@gmail.com>
2021-05-01 10:28:11 -0700

    🔨 Implement removing from trie

--- src/server/index.ts
+++ src/server/index.ts
@@ -41,6 +41,53 @@ const addToTrie: (trie: Trie, phraseToAdd: string) => void = (
   saveToDatabase(globalTrie);
 };
 
+const removeFromTrie: (trie: Trie, phraseToRemove: string) => void = (
+  trie,
+  phraseToRemove,
+) => {
+  let currentTrie = trie;
+  let newTailNode = trie;
+  let indexCharacterToDelete =
+    phraseToRemove.length > 0 ? phraseToRemove[0] : null;
+
+  for (
+    let characterIndex = 0;
+    characterIndex < phraseToRemove.length;
+    ++characterIndex
+  ) {
+    // Walk through
+    const currentCharacter = phraseToRemove[characterIndex];
+    if (
+      typeof currentTrie[currentCharacter] === 'undefined' ||
+      currentTrie[currentCharacter] === null
+    ) {
+      return;
+    }
+
+    // Don't delete any nodes that have other solutions
+    const previousTrie = currentTrie;
+    const previousTrieSolutionDefined =
+      typeof previousTrie['**'] !== 'undefined' && previousTrie['**'] !== null;
+    currentTrie = currentTrie[currentCharacter] as Trie;
+    if (
+      previousTrie['**'] === true ||
+      (!previousTrieSolutionDefined && Object.keys(previousTrie).length > 1)
+    ) {
+      newTailNode = previousTrie;
+      indexCharacterToDelete = currentCharacter;
+    }
+  }
+
+  // If this is a valid word in the trie
+  if (currentTrie['**'] === true && indexCharacterToDelete !== null) {
+    // Delete if there are no children
+    if (Object.keys(currentTrie).length < 2) {
+      delete newTailNode[indexCharacterToDelete];
+    }
+  }
+  saveToDatabase(globalTrie);
+};
+
 // ! REMOVE ME BEFORE PRODUCTION. For development purposes only
 const printTrie = () => {
   console.log(JSON.stringify(globalTrie, undefined, 2));
@@ -83,6 +130,21 @@ app.post('/add', (req, res) => {
   res
     .status(201)
     .send({ success: true, message: 'Added to trie successfully' });
+});
+app.delete('/delete', (req, res) => {
+  console.log('Request to delete received');
+  const { keyword } = req.body;
+
+  // Input validation
+  if (typeof keyword === 'undefined' || keyword === null) {
+    res.status(400).send({ success: false, message: 'Must specify keyword' });
+    return;
+  }
+
+  removeFromTrie(globalTrie, keyword);
+  res
+    .status(200)
+    .send({ success: true, message: 'Deleted from trie successfully' });
 });
 
 app.listen(process.env.PORT, () => {
